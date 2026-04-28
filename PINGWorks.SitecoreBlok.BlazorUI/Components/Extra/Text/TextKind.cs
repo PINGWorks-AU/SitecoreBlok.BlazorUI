33378175c2466128599c1f26e62cb4121712371b
Richard Hauer <richard.hauer@ping-works.com.au>
2026-04-28 21:28:30 +1000

    Update Text component with a range of properties to make it more useful.

---
+++ PINGWorks.SitecoreBlok.BlazorUI/Components/Extra/Text/TextKind.cs
@@ -0,0 +1,6 @@
+namespace PINGWorks.SitecoreBlok.BlazorUI;
+
+/// <summary>
+/// HTML element kind rendered by the <see cref="Text"/> component.
+/// </summary>
+public enum TextKind { P, Span, Div, H1, H2, H3, H4, H5, H6 }
